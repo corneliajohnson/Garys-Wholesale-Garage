d6466156e75043e412155f3ae48e08ecec2b809b
corneliajohnson <corneliaj91@gmail.com>
2020-11-30 11:38:50 -0500

    Created a Drive() method in the Vehicle class

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace GarysWholeGarage
   {
     static void Main(string[] args)
     {
+      //Create an instance of each vehicle.
+      //Define a different value for each vehicle's properties.
       var myCessna = new Cessna()
       {
         MainColor = "white",
@@ -30,6 +32,12 @@ namespace GarysWholeGarage
         MaximumOccupancy = 2,
         BatteryKWh = 6.5
       };
+
+      //Create a Drive() method in the Vehicle class.
+      myCessna.Drive();
+      myRam.Drive();
+      myTesla.Drive();
+      myZero.Drive();
     }
   }
 }
